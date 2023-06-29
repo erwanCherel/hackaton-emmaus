46721de28f4473a5a46fa8889ee4d1cb31125c42
Luc <lefebureluc.42@gmail.com>
2023-06-29 09:55:49 +0200

    solution dev

---
+++ frontend/src/pages/RetailPhone.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "@chakra-ui/react";
+
+export default function RetailPhone() {
+  //   const [phone, setPhone] = useState();
+  //   const [memory, setMemory] = useState();
+
+  return (
+    <Stack>
+      <h1>Coucou !</h1>
+    </Stack>
+  );
+}
